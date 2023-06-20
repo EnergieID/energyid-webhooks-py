519b84c90a43603ea3514e4b165ee7c29538e072
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2023-06-20 18:28:52 +0200

    payload, policy

--- src/energyid_webhooks/client.py
+++ src/energyid_webhooks/client.py
@@ -1,9 +1,11 @@
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 import aiohttp
 
 import requests
 
 from .metercatalog import MeterCatalog
+from .payload import WebhookPayload
+from .webhookpolicy import WebhookPolicy
 
 
 class WebhookClient:
@@ -14,12 +16,20 @@ class WebhookClient:
         self.session = session if session is not None else requests.Session()
 
         self._meter_catalog = None
+        self._webhook_policy = None
 
-    def get(self) -> Dict:
+    @property
+    def policy(self) -> Dict:
+        if self._webhook_policy is None:
+            self._webhook_policy = self.get_policy()
+        return self._webhook_policy
+    
+    def get_policy(self) -> WebhookPolicy:
         r = self.session.get(url=self.webhook_url)
         r.raise_for_status()
-        return r.json()
-    
+        self._webhook_policy = WebhookPolicy(r.json())
+        return self._webhook_policy
+
     def post(self, data: Dict):
         r = self.session.post(url=self.webhook_url, json=data)
         r.raise_for_status()
@@ -36,16 +46,26 @@ class WebhookClient:
         r.raise_for_status()
         return MeterCatalog(r.json())
     
+    def post_payload(self, payload: WebhookPayload):
+        self.post(payload.to_dict())
+    
 
 class WebhookClientAsync(WebhookClient):
     def __init__(self, webhook_url: str, session: Optional[aiohttp.ClientSession] = None):
         session = session if session is not None else aiohttp.ClientSession()
         super(WebhookClientAsync, self).__init__(webhook_url=webhook_url, session=session)
 
-    async def get(self) -> Dict:
+    @property
+    async def policy(self) -> Dict:
+        if self._webhook_policy is None:
+            self._webhook_policy = await self.get_policy()
+        return self._webhook_policy
+    
+    async def get_policy(self) -> WebhookPolicy:
         async with self.session.get(url=self.webhook_url) as r:
             r.raise_for_status()
-            return await r.json()
+            self._webhook_policy = WebhookPolicy(await r.json())
+            return self._webhook_policy
         
     async def post(self, data: Dict):
         async with self.session.post(url=self.webhook_url, json=data) as r:
@@ -63,3 +83,6 @@ class WebhookClientAsync(WebhookClient):
         if self._meter_catalog is None:
             self._meter_catalog = await self.get_meter_catalog()
         return self._meter_catalog
+    
+    async def post_payload(self, payload: WebhookPayload):
+        await self.post(payload.to_dict())
