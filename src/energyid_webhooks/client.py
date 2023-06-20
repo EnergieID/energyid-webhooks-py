edb2e4320d138a926a540f05d23a9dfafa722a6e
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2023-06-20 13:48:31 +0200

    add meter catalog

--- src/energyid_webhooks/client.py
+++ src/energyid_webhooks/client.py
@@ -1,13 +1,19 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 import aiohttp
 
 import requests
 
+from .metercatalog import MeterCatalog
+
 
 class WebhookClient:
+    meter_catalog_url = "https://api.energyid.eu/api/v1/catalogs/meters"
+
     def __init__(self, webhook_url: str, session: Optional[requests.Session] = None):
         self.webhook_url = webhook_url
         self.session = session if session is not None else requests.Session()
+
+        self._meter_catalog = None
 
     def get(self) -> Dict:
         r = self.session.get(url=self.webhook_url)
@@ -18,6 +24,17 @@ class WebhookClient:
         r = self.session.post(url=self.webhook_url, json=data)
         r.raise_for_status()
         return
+    
+    @property
+    def meter_catalog(self) -> MeterCatalog:
+        if self._meter_catalog is None:
+            self._meter_catalog = self.get_meter_catalog()
+        return self._meter_catalog
+    
+    def get_meter_catalog(self) -> MeterCatalog:
+        r = self.session.get(url=self.meter_catalog_url)
+        r.raise_for_status()
+        return MeterCatalog(r.json())
     
 
 class WebhookClientAsync(WebhookClient):
@@ -34,3 +51,15 @@ class WebhookClientAsync(WebhookClient):
         async with self.session.post(url=self.webhook_url, json=data) as r:
             r.raise_for_status()
             return
+        
+    async def get_meter_catalog(self) -> MeterCatalog:
+        async with self.session.get(url=self.meter_catalog_url) as r:
+            r.raise_for_status()
+            d = await r.json()
+            return MeterCatalog(d)
+        
+    @property
+    async def meter_catalog(self) -> MeterCatalog:
+        if self._meter_catalog is None:
+            self._meter_catalog = await self.get_meter_catalog()
+        return self._meter_catalog
